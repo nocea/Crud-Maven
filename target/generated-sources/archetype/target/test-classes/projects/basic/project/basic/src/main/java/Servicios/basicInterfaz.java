d328fdf6193307e325aa4d0b469330dd1f2234da
Mario Nocea Cabrera <mnoceacabrera@gmail.com>
2023-10-20 12:36:07 +0200

    v.2-{20102023}

---
+++ target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/Servicios/basicInterfaz.java
@@ -0,0 +1,27 @@
+package Servicios;
+import java.util.ArrayList; 
+import java.sql.Connection;
+import Dtos.*;
+public interface basicInterfaz {
+	/**
+	 * Método que devuelve un array para mostrar los libros que existen
+	 * en la base de datos
+	 * @param conexion
+	 * */
+	public void MostrarLibros(Connection conexion);
+	/**
+	 * Método para actualizar los datos de un registro de la base de datos
+	 * @param conexion
+	 * */
+	public void ActualizarLibros(Connection conexion);
+	/**
+	 * Método para crear un nuevo registro en la base de datos
+	 * @param conexion
+	 * */
+	public void CrearLibros(Connection conexion);
+	/**
+	 * Método para borrar un registro de la base de datos
+	 * @param conexion
+	 * */
+	public void BorrarLibros(Connection conexion);
+}
